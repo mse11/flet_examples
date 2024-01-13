5f29eed9301ded9e81c487e3e45f0e1df1a5ef4a
Michal Sestrienka <sestosko@gmail.com>
2024-01-13 06:54:00 +0100

    tutorial basic hello_v00

---
+++ python/apps/hello-world/hello_00_colors.py
@@ -0,0 +1,26 @@
+import flet as ft
+
+
+# There are 2 ways to define color property value in Flet: hex value and named colors.
+# 1. Hex value
+#   a) #rrggbb (0xeeggbb)                     WITHOUT aa (opacity) == ff (not transparent).
+#   b) #aarrggbb (0xaarrggbb)                 WITH    aa (opacity)
+#   c) ft.colors.with_opacity(0.5, '#rrggbb') (opacity - between 0.0 (completely transparent) and 1.0 (not transparent))
+# 2. Named colors
+# 	- Material Design theme colors https://m3.material.io/styles/color/roles/color-roles
+#   - colors palettes https://m2.material.io/design/color/the-color-system.html#color-usage-and-palettes)
+#   a) string
+#   b) flet.colors module
+#   c) ft.colors.with_opacity(0.5, flet.colors.XXX) (opacity - 0.0 (completely transparent) and 1.0 (not transparent))
+
+def main(page: ft.Page):
+    page.add(ft.Text("HEX  : '#000000'"                       , color='#000000'))
+    page.add(ft.Text("HEX  : '#40000000'"                     , color='#40000000'))
+    page.add(ft.Text("HEX  : with_opacity(0.5, '#000000')"    , color=ft.colors.with_opacity(0.5, '#000000')))
+    page.add(ft.Text("named: 'red'"                           , color='red'))
+    page.add(ft.Text("named: 'red,0.5'"                       , color='red,0.5'))
+    page.add(ft.Text("named: ft.colors.RED"                   , color=ft.colors.RED))
+    page.add(ft.Text("named: ft.colors.RED_200"               , color=ft.colors.RED_200))
+    page.add(ft.Text("named: with_opacity(0.5, ft.colors.RED)", color=ft.colors.with_opacity(0.5, ft.colors.RED)))
+
+ft.app(main)
