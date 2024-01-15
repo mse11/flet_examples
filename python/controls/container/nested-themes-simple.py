d93e47f7d618182d180fd747e6e92b7843e09593
Michal Sestrienka <sestosko@gmail.com>
2024-01-15 13:01:06 +0100

    controls nested-themes-simple

--- python/controls/container/nested-themes-simple.py
+++ python/controls/container/nested-themes-simple.py
@@ -1,5 +1,5 @@
 import flet as ft
-
+from theme_colors import theme_colors_containers
 
 def main(page: ft.Page):
     # Yellow page theme with SYSTEM (default) mode
@@ -7,7 +7,34 @@ def main(page: ft.Page):
         color_scheme_seed=ft.colors.YELLOW,
     )
 
-    page.add(
+    c_orig = ft.Column(controls=[
+        # Page theme
+        ft.Container(
+            content=ft.ElevatedButton("Page theme button"),
+            #bgcolor=ft.colors.SURFACE_VARIANT,
+            padding=20,
+            width=300,
+        ),
+        # Inherited theme with primary color overridden
+        ft.Container(
+            #theme=ft.Theme(color_scheme=ft.ColorScheme(primary=ft.colors.PINK)),
+            content=ft.ElevatedButton("Inherited theme with primary color overridden"),
+            #bgcolor=ft.colors.SURFACE_VARIANT,
+            padding=20,
+            width=300,
+        ),
+        # "Inherited theme with primary color overridden DARK THEME"
+        ft.Container(
+            #theme=ft.Theme(color_scheme_seed=ft.colors.PINK),
+            #theme_mode=ft.ThemeMode.DARK,
+            content=ft.ElevatedButton("Inherited theme with primary color overridden DARK THEME"),
+            #bgcolor=ft.colors.SURFACE_VARIANT,
+            padding=20,
+            width=300,
+        ),
+    ])
+
+    c_custom = ft.Column(controls=[
         # Page theme
         ft.Container(
             content=ft.ElevatedButton("Page theme button"),
@@ -18,20 +45,25 @@ def main(page: ft.Page):
         # Inherited theme with primary color overridden
         ft.Container(
             theme=ft.Theme(color_scheme=ft.ColorScheme(primary=ft.colors.PINK)),
-            content=ft.ElevatedButton("Inherited theme button"),
+            content=ft.ElevatedButton("Inherited theme with primary color overridden"),
             bgcolor=ft.colors.SURFACE_VARIANT,
             padding=20,
             width=300,
         ),
-        # Unique always DARK theme
+        # "Inherited theme with primary color overridden DARK THEME"
         ft.Container(
-            theme=ft.Theme(color_scheme_seed=ft.colors.INDIGO),
+            theme=ft.Theme(color_scheme_seed=ft.colors.PINK),
             theme_mode=ft.ThemeMode.DARK,
-            content=ft.ElevatedButton("Unique theme button"),
+            content=ft.ElevatedButton("Inherited theme with primary color overridden DARK THEME"),
             bgcolor=ft.colors.SURFACE_VARIANT,
             padding=20,
             width=300,
         ),
+    ])
+
+    page.add(
+        ft.Row(controls=theme_colors_containers()),
+        ft.Row(controls=[c_orig, c_custom]),
     )
 
 
