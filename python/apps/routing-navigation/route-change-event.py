6d5212abd270137d585765c641bd475f05a22472
Michal Sestrienka <sestosko@gmail.com>
2024-01-27 06:30:10 +0100

    APP routing - route-change-event v01

--- python/apps/routing-navigation/route-change-event.py
+++ python/apps/routing-navigation/route-change-event.py
@@ -3,13 +3,14 @@ from flet import Page, Text
 
 
 def main(page: Page):
-    page.add(Text(f"Initial route: {page.route}"))
+    page.add(Text(f"Initial route: {page.route} views: {page.views}"))
 
     def route_change(e):
-        page.add(Text(f"New route: {e.route}"))
+        page.add(Text(f"New route: {e.route} views: {page.views}"))
 
     page.on_route_change = route_change
     page.update()
 
 
-flet.app(target=main, view=flet.WEB_BROWSER)
+flet.app(target=main, view=flet.WEB_BROWSER)  # default: route_url_strategy="path", 127.0.0.1:port,  127.0.0.1:port/path1
+# flet.app(target=main, view=flet.WEB_BROWSER, route_url_strategy="hash")  # 127.0.0.1:port,  127.0.0.1:port/#/path1
